c085ef7bc01e9b666d1ff2831fa5c43b0102a4e7
Andrew <andrewk17@gmail.com>
2016-10-01 14:56:59 -0700

    Move Ethereum Server from main webserver

---
+++ services/readSvc.js
@@ -0,0 +1,24 @@
+const contractHelper = require('../contracts/contractHelpers.js');
+const web3Connection = require('../web3.js');
+
+const web3 = web3Connection.web3;
+
+const readSvc = {
+  readEvent: (eventContractAddress) => {
+    const eventContractInstance = web3.eth.contract(contractHelper.contractObj).at(eventContractAddress);
+    const eventObj = {
+      organizer: eventContractInstance.organizer.toString(),
+      numAttendees: eventContractInstance.numAttendees().toString(),
+      attendeesPaid: eventContractInstance.attendeesPaid(), // TODO: Parse this mapping variable correctly
+      quota: eventContractInstance.quota().toString(),
+      price: eventContractInstance.price().toString(),
+      eventName: eventContractInstance.eventName().toString(),
+      eventCreateDateTime: new Date(parseInt(eventContractInstance.eventCreateDateTime().toString(), 10)),
+      eventStartDateTime: new Date(parseInt(eventContractInstance.eventStartDateTime().toString(), 10)),
+      eventEndDateTime: new Date(parseInt(eventContractInstance.eventEndDateTime().toString(), 10)),
+    };
+    return eventObj;
+  },
+};
+
+module.exports = readSvc;
