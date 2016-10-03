3e14265894e7804d0e4d07e42eb5809ac4bcb193
lentan <lentan1029@gmail.com>
2016-10-03 15:02:30 -0700

    Revert "changed files"
    This reverts commit ddeb7b1834a5ddff6a352a0c03ad44d306387d47.

---
+++ database/dbController.js
@@ -0,0 +1,50 @@
+const eventModel = require('./models/event.js');
+const Promise = require('bluebird');
+
+const controller = {
+  findEvent: req => new Promise((fulfill, reject) => {
+    eventModel.findOne({
+      where: {
+        eventName: req.query.eventName,
+      },
+    }).then((event) => {
+      if (event) {
+        fulfill(event);
+      } else {
+        reject('No events found');
+      }
+    });
+  }),
+  createEvent: (eventObj, res) => {
+    eventModel.create({
+      eventName: eventObj.eventName,
+      contractAddress: eventObj.contractAddress,
+      createDateTime: eventObj.createDateTime,
+      startDateTime: eventObj.startDateTime,
+      endDateTime: eventObj.endDateTime })
+    .then((event) => {
+      console.log(`${event.eventName} added to DB`);
+      res.sendStatus(200);
+    }).catch((err) => {
+      console.log(err);
+      res.status(500).send(err);
+    });
+  },
+  getAllEvents: () => new Promise((fulfill, reject) => {
+    eventModel.findAll({
+      where: {
+        startDateTime: {
+          $gte: new Date(),
+        },
+      },
+    }).then((events) => {
+      if (events) {
+        fulfill(events);
+      } else {
+        reject('No events found');
+      }
+    });
+  }),
+};
+
+module.exports = controller;
