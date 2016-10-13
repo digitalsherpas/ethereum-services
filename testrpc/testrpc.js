3d1bc4d40866ceef265fffa1928b55d8eb4692b3
lentan <lentan1029@gmail.com>
2016-10-12 17:37:34 -0700

    move endpoints into config

--- testrpc/testrpc.js
+++ testrpc/testrpc.js
@@ -2,7 +2,7 @@ const config = require('../config');
 const TestRPC = require('ethereumjs-testrpc');
 
 const server = TestRPC.server();
-server.listen(process.env.BLOCKCHAIN_PORT || config.BLOCKCHAIN_PORT, (err, blockchain) => {
+server.listen(config.BLOCKCHAIN_PORT, (err, blockchain) => {
   console.log(`TEST RPC server running on http://localhost:${config.BLOCKCHAIN_PORT}`);
   console.log('Account Addresses:');
   console.log(Object.keys(blockchain.accounts));
