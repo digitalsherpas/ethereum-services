1c056573d98e7404200b3a48dd6492c4d4249d0a
lentan <lentan1029@gmail.com>
2016-10-12 17:56:14 -0700

    add config dependency

--- web3.js
+++ web3.js
@@ -1,4 +1,5 @@
 const Web3 = require('web3');
+const config = require('./config');
 
 const connectionString = config.ETHEREUM_CONNECTION_STRING || 'http://localhost:8545';
 const web3 = new Web3(new Web3.providers.HttpProvider(connectionString));
